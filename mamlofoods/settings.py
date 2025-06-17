a90bca7b957b4e26d075f07feb44513ccb65b223
Kim-Onesmus <kimones1122@gmail.com>
2025-06-17 08:46:35 +0300

    updates

--- mamlofoods/settings.py
+++ mamlofoods/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&!m4q*k%rr6-tuqy!u*afs@07=qocj^q4a#rkkyynj@#u7@fqc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,10 +123,6 @@ if DEBUG:
     STATIC_URL = 'static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_DIR = os.path.join(BASE_DIR, 'app/static')
-
-
-    MEDIA_URL = 'media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 else:
     STATIC_URL = 'static/'
