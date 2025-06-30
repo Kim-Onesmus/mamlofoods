2e31f7357314b0b98e836ce6f59f7f5d1dfd0207
Kim-Onesmus <kimones1122@gmail.com>
2025-06-30 23:03:58 +0300

    updates

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Blog
+from django.utils.html import format_html
+from django.utils.text import Truncator
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('short_title', 'author', 'thumbnail', 'published_at', 'is_recent')
+    readonly_fields = ('slug', 'published_at', 'updated_at')
+    search_fields = ('title', 'content', 'author__username')
+    list_filter = ('published_at', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('-published_at',)
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'author', 'featured_image', 'content')
+        }),
+        ('Timestamps', {
+            'fields': ('published_at', 'updated_at')
+        }),
+    )
+
+    def short_title(self, obj):
+        return Truncator(obj.title).chars(30)
+    short_title.short_description = 'Title'
+
+    def thumbnail(self, obj):
+        if obj.featured_image:
+            return format_html('<img src="{}" width="60" height="40" style="object-fit:cover; border-radius:4px;" />', obj.featured_image.url)
+        return "-"
+    thumbnail.short_description = 'Image'
+
+    def is_recent(self, obj):
+        return obj.published_at.date() == obj.updated_at.date()
+    is_recent.boolean = True
+    is_recent.short_description = 'Updated Today?'
--- app/models.py
+++ app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+import os
+import uuid
+from django.db import models
+from django.utils.text import slugify
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
+
+
+def upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    slug = slugify(instance.title)[:50]
+    return f'blog/{slug}.{ext}'
+
+
+class Blog(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    featured_image = models.ImageField(upload_to=upload_to)
+    author = models.CharField(max_length=255)
+    content = RichTextUploadingField()
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-published_at']
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)[:50]
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
