bb2f2d804b66194e744e81bc19dd02ef394e6328
Kim-Onesmus <kimones1122@gmail.com>
2025-07-03 16:09:55 +0300

    updates

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.landing, name='landing'),
+    path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('submit-contact/', views.submit_contact_form, name='submit_contact_form'),
