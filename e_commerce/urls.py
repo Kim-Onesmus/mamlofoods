5aa5c6b09757043194c1d85b4af631277222a4d3
Kim-Onesmus <kimones1122@gmail.com>
2025-07-19 20:11:06 +0300

    updates

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='home'),
+    path('product/<str:slug>/', views.ProductDetails, name='product_details'),
+    path('cart/', views.Cart, name='cart'),
+    path('checkout/', views.Checkout, name='checkout'),
+    path('orders/', views.MyOrders, name='order_page'),
+    path('register/', views.Register, name='register_page'),
+    path('login/', views.Login, name='login_page'),
+    path('account/', views.Account, name='account'),
 ]
--- e_commerce/views.py
+++ e_commerce/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request, 'e_commerce/home.html')
+
+def ProductDetails(request):
+    return render(request, 'e_commerce/product_details.html')
+
+def Cart(request):
+    return render(request, 'e_commerce/cart.html')
+
+def Checkout(request):
+    return render(request, 'e_commerce/checkout.html')
+
+def MyOrders(request):
+    return render(request, 'e_commerce/my_orders.html')
+
+def Register(request):
+    return render(request, 'e_commerce/register.html')
+
+def Login(request):
+    return render(request, 'e_commerce/login.html')
+
+def Account(request):
+    return render(request, 'e_commerce/product_details.html')
