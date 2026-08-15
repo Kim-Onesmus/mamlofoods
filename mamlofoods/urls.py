4ddfa26aa7387547cbf887eccf18f4cc52e54392
Kim-Onesmus <kimones1122@gmail.com>
2026-08-15 14:35:35 +0300

    updates

--- mamlofoods/urls.py
+++ mamlofoods/urls.py
@@ -6,8 +6,8 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('', include('app.urls')),
-    path('store/', include('e_commerce.urls'))
+    path('app/', include('app.urls')),
+    path('', include('e_commerce.urls'))
 ]
 
 if settings.DEBUG:
